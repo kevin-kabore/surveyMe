859bea9fa3c306667eb6f1deef0ad91d0df262ce
Kevin Kabore <kevin.s.kabore@gmail.com>
2017-09-10 14:59:30 -0700

    updates callback to make sure only 1 user is created

--- server/services/passport.js
+++ server/services/passport.js
@@ -13,7 +13,14 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      new User({ googleId: profile.id }).save();
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          // already have a record
+        } else {
+          //no recorde make new User
+          new User({ googleId: profile.id }).save();
+        }
+      });
     }
   )
 );
