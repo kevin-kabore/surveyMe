0f50a76e9b89f0b8253af48befa8a12fa2d8f9c7
Kevin Kabore <kevin.s.kabore@gmail.com>
2017-09-09 22:30:40 -0700

    creates User model class with mongoose Schema

--- server/index.js
+++ server/index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
 require('./services/passport');
+require('./models/User');
 
 mongoose.connect(keys.mongoURI, { useMongoClient: true });
 // const db = mongoose.connection;
 // db.on('error', console.error.bind(console, 'MongoDB connection error: '));
 
 const app = express();
+
 require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
