192787b039d9a10e0184861534b0952877d02bb3
Kevin Kabore <kevin.s.kabore@gmail.com>
2017-09-09 17:32:54 -0700

    sets up remote mongo database and connects with mongoose

--- server/index.js
+++ server/index.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 require('./services/passport');
+
+mongoose.connect(keys.mongoURI, { useMongoClient: true });
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'MongoDB connection error: '));
 
 const app = express();
 require('./routes/authRoutes')(app);
