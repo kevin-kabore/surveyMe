67d4c9e62b862f79c4c2b52794e293c7203bef8d
Kevin Kabore <kevin.s.kabore@gmail.com>
2017-09-28 14:42:25 -0700

    Completes header logic

---
+++ server/client/src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>SurveyMe!</h1>
+      Survey your users for feedback
+    </div>
+  );
+};
+
+export default Landing;
