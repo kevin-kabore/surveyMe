587913477369af69907d58ea7b60e1401bda9fbd
Kevin Kabore <kevin.s.kabore@gmail.com>
2017-09-27 18:11:29 -0700

    completes oauth flow for login

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,6 +1,30 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <div>
+            <li>
+              <a href="/auth/google">Login with Google</a>
+            </li>
+            <li>
+              <a href="/auth/facebook">Login with facebook</a>
+            </li>
+          </div>
+        );
+      default:
+        return (
+          <li>
+            <a href="/api/logout">Logout</a>
+          </li>
+        );
+    }
+  }
   render() {
     return (
       <nav>
@@ -9,9 +33,7 @@ class Header extends Component {
             SurveyMe
           </a>
           <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li>
-              <a>Login with Google</a>
-            </li>
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
@@ -19,4 +41,8 @@ class Header extends Component {
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
