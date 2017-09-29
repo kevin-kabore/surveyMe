042f1913574fd51b1f2bb20cda936799be186434
Kevin Kabore <kevin.s.kabore@gmail.com>
2017-09-28 22:57:13 -0700

    adds credits to user model and adds credit on post req to stripe api

--- server/models/User.js
+++ server/models/User.js
@@ -4,7 +4,8 @@ const { Schema } = mongoose;
 
 const userSchema = new Schema({
   facebookId: String,
-  googleId: String
+  googleId: String,
+  credits: { type: Number, default: 0 }
 });
 
 mongoose.model('users', userSchema);
