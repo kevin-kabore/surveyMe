2b700a698246136c5bd28cf2b1cac471c3b107c9
Kevin Kabore <kevin.s.kabore@gmail.com>
2017-09-28 22:35:45 -0700

    adds body-parser to server as well as stripe and creates charge

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
+const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
 const express = require('express');
 const mongoose = require('mongoose');
-const cookieSession = require('cookie-session');
 const passport = require('passport');
 
 const keys = require('./config/keys');
@@ -13,6 +14,7 @@ mongoose.connect(keys.mongoURI, { useMongoClient: true });
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -23,6 +25,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
