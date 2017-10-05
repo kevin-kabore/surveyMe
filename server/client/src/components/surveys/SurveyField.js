f7b0a24784e7d981524568c0e1b9165e8e4aa416
Kevin Kabore <kevin.s.kabore@gmail.com>
2017-10-05 12:41:53 -0700

    adds form validation for title field

--- server/client/src/components/surveys/SurveyField.js
+++ server/client/src/components/surveys/SurveyField.js
@@ -4,11 +4,12 @@ import React from 'react';
 // input = props.input, label = props.label coming from Field on SurveyForm as props
 // {...input} same as all: onBlur={input.onBlur} onChange={input.onChange} etc...
 // means pass all properties on input
-export default ({ input, label }) => {
+export default ({ input, label, meta: { error, touched } }) => {
   return (
     <div>
       <label>{label}</label>
       <input {...input} />
+      {touched && error}
     </div>
   );
 };
