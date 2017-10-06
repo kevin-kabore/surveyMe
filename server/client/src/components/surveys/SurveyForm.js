62d3dfe7b49f711a0cc66db46a5a87a5d7bc9c47
Kevin Kabore <kevin.s.kabore@gmail.com>
2017-10-06 12:37:41 -0700

    wires up funcitonality

--- server/client/src/components/surveys/SurveyForm.js
+++ server/client/src/components/surveys/SurveyForm.js
@@ -6,28 +6,11 @@ import { Link } from 'react-router-dom';
 
 import SurveyField from './SurveyField';
 import validateEmails from '../../utils/validateEmails';
+import formFields from './formFields';
 
-const FIELDS = [
-  {
-    label: 'Survey Title',
-    name: 'title',
-    noValueError: 'You must provide a title'
-  },
-  {
-    label: 'Subject Line',
-    name: 'subject',
-    noValueError: 'You must provide a subject'
-  },
-  {
-    label: 'Email Body',
-    name: 'body',
-    noValueError: 'You must provide an email body'
-  },
-  { label: 'Recipient List', name: 'emails' }
-];
 class SurveyForm extends Component {
   renderFields() {
-    return _.map(FIELDS, ({ label, name }) => {
+    return _.map(formFields, ({ label, name }) => {
       return (
         <Field
           key={name}
@@ -38,45 +21,6 @@ class SurveyForm extends Component {
         />
       );
     });
-    // lodash.map(Array, instanceName callback) same as .map() but faster
-    // return FIELD.map(field => {
-    //   return (
-    //     <Field
-    //       label={field.label}
-    //       name={field.name}
-    //       type="text"
-    //       component={SurveyField}
-    //     />
-    //   )
-    // })
-    // return (
-    //   <div>
-    //     <Field
-    //       label="Survey Title"
-    //       type="text"
-    //       name="title"
-    //       component={SurveyField}
-    //     />
-    //     <Field
-    //       label="Subject Line"
-    //       type="text"
-    //       name="subject"
-    //       component={SurveyField}
-    //     />
-    //     <Field
-    //       label="Email Body"
-    //       type="text"
-    //       name="body"
-    //       component={SurveyField}
-    //     />
-    //     <Field
-    //       label="Recipient List"
-    //       type="text"
-    //       name="emails"
-    //       component={SurveyField}
-    //     />
-    //   </div>
-    // );
   }
   render() {
     return (
@@ -99,26 +43,17 @@ function validate(values) {
   const errors = {};
 
   errors.emails = validateEmails(values.emails || '');
-
   // FILEDS.forEach()
   // destructure properties off of value
-  _.each(FIELDS, ({ name, noValueError }) => {
+  _.each(formFields, ({ name, noValueError }) => {
     if (!values[name]) {
       errors[name] = noValueError;
     }
   });
-  // if (!values.title) {
-  //   errors.title = 'You must provide a title';
-  // }
-  // if (!values.subject) {
-  //   errors.subject = 'You must provide a subject';
-  // }
-  // if (!values.body) {
-  //   errors.body = 'You must provide a body';
-  // }
   return errors;
 }
 export default reduxForm({
   validate,
-  form: 'surveyForm'
+  form: 'surveyForm',
+  destroyOnUnmount: false
 })(SurveyForm);
--- server/client/src/components/surveys/SurveyNew.js
+++ server/client/src/components/surveys/SurveyNew.js
@@ -5,16 +5,15 @@ import SurveyForm from './SurveyForm';
 import SurveyFormReview from './SurveyFormReview';
 
 class SurveyNew extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state({ formReview: false });
-  // }
-  // create-react-app allows for:
   state = { showFormReview: false };
 
   renderContent() {
     if (this.state.showFormReview) {
-      return <SurveyFormReview />;
+      return (
+        <SurveyFormReview
+          onCancel={() => this.setState({ showFormReview: false })}
+        />
+      );
     }
 
     return (
